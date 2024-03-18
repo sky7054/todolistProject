6d54c78475c7efebfd5db910304850f7343bb1e7
Akash Yadav <akashcse7054@gmail.com>
2024-03-18 23:56:55 +0530

    fetch data from local storage

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 const mainTodoEle = document.querySelector(".todo-lists-ele");
 const inputField = document.getElementById("inputValue");
 
+const getTodoListfromLocal = () => {
+  return JSON.parse(localStorage.getItem("addTodoListValue"));
+};
 
-let localTodoLists = [];
+//Adding add to list dyanamically
+
+const addToDynamicElement = (currEle) => {
+  const divElement = document.createElement("div"); // create div tag
+  divElement.classList.add("main-todo-div"); // insert class name in div
+  divElement.innerHTML = ` <li>${currEle}</li> <button class="deleteBtn">Delete</button>`;
+  mainTodoEle.append(divElement);
+};
+
+let localTodoLists = getTodoListfromLocal() || []; // store input value
 
 const addTodoList = (e) => {
-    e.preventDefault();
-    
-    const todoListValues = inputField.value.trim();
-    localTodoLists.push(todoListValues);
-    localTodoLists = [...new Set(localTodoLists)];
-    localStorage.setItem("addTodoListValue",JSON.stringify(localTodoLists))
-    console.log(localTodoLists);
+  e.preventDefault();
 
-    const divElement  = document.createElement("div");
-    divElement.classList.add("main-todo-div");
-    divElement.innerHTML = ` <li>${inputField.value}</li> <button class="deleteBtn">Delete</button>`;
-    mainTodoEle.append(divElement);
-    inputField.value ="";
-    
-}
+ 
+
+  const todoListValues = inputField.value.trim(); // trim methods remove first and last space
+  inputField.value ="";
+
+  if(todoListValues !== ""  && !localTodoLists.includes(todoListValues)){
+  localTodoLists.push(todoListValues);
+  localTodoLists = [...new Set(localTodoLists)]; //its prevent duplicate value in array
+  localStorage.setItem("addTodoListValue", JSON.stringify(localTodoLists));
+  
+
+  // const divElement  = document.createElement("div"); // create div tag
+  // divElement.classList.add("main-todo-div"); // insert class name in div
+  // divElement.innerHTML = ` <li>${inputField.value}</li> <button class="deleteBtn">Delete</button>`;
+  // mainTodoEle.append(divElement);
+  // 
+
+  addToDynamicElement(todoListValues);
+  }
+};
+
+const showTodoList = () => {
+  // console.log(localTodoLists);
+  localTodoLists.forEach((currEle) => {
+    addToDynamicElement(currEle);
+  });
+};
+
+showTodoList();
 
 document.querySelector(".btn").addEventListener("click", (e) => {
-    addTodoList(e);
-})
+  addTodoList(e);
+});
